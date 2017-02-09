1a41c95d5e58469fa5a7b3eec82b78ffb797ba7d
Ashok Tulachan <tulachanashok@Ashoks-MBP.fios-router.home>
2017-02-08 20:57:13 -0500

    Updates

--- server/server.js
+++ server/server.js
@@ -73,9 +73,11 @@
 // });
 
 //Resource Creation End Point - Section 7 Lecture 73
-var express = require('express');
-var bodyParser = require('body-parser');
+const _ = require('lodash');
+const express = require('express');
+const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
+
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
@@ -147,6 +149,34 @@ app.delete('/todos/:id', (req, res) => {
   }).catch((e) => res.status(400).send());
 })
 
+
+//PATCH
+app.patch('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  var body = _.pick(req.body, ['text', 'completed'])
+
+  //Validate id using isValid
+  if (!ObjectID.isValid(id)) {
+    console.log('ID not valid');
+    return res.status(404).send();
+  }
+
+  if(_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
+  })
+});
 
 app.listen(port, () => {
   console.log(`Started on port ${port}`);
