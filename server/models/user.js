d29088f5008fad4b5a060e4ae585b9ced16f7110
Ashok Tulachan <tulachanashok@Ashoks-MBP.fios-router.home>
2017-02-12 13:42:01 -0500

    Add tests for DELETE /users/me/token

--- server/models/user.js
+++ server/models/user.js
@@ -63,6 +63,17 @@ UserSchema.methods.generateAuthToken = function () {
   });
 };
 
+UserSchema.methods.removeToken = function (token) {
+  var user = this;
+  return user.update({
+    $pull: {
+      tokens: {
+        token: token
+      }
+    }
+  });
+};
+
 //Model method
 UserSchema.statics.findByToken = function (token) {
   var User = this;
